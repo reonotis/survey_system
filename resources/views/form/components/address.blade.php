3da3bd7061b9ccdcdffe066ce93b8b0f9c9fc26d
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2025-11-18 23:07:09 +0900

    住所の設定を可能にする

---
+++ resources/views/form/components/address.blade.php
@@ -0,0 +1,61 @@
+@php
+    $details = json_decode($form_item->details ?? '{}', true);
+    $post_code_use_type = $details['post_code_use_type'] ?? 0;
+    $address_separate_type = $details['address_separate_type'] ?? 0;
+
+    if($address_separate_type == App\Consts\CommonConst::ADDRESS_SEPARATE) {
+        $on_keyup = "AjaxZip3.zip2addr('zip21','zip22','pref21','address21','street21');";
+    } elseif($address_separate_type == App\Consts\CommonConst::ADDRESS_NON_SEPARATE)  {
+        $on_keyup = "AjaxZip3.zip2addr('zip21','zip22','address','address');";
+    }
+
+@endphp
+
+@push('scripts')
+    <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+@endpush
+
+<div class="support-msg">
+    {!! nl2br($form_item->annotation_text) !!}
+</div>
+
+<div>
+    <div class="form-item-detail-content">
+        <div class="">
+
+            @if($post_code_use_type)
+                <div class="flex-start-center gap-2 mb-1">
+                    <x-input-text name="zip21" id="zip21" class="w-20" :value="old('zip21')" placeholder='100'
+                    onKeyUp="{{ $on_keyup }}" :error="$errors->get('zip21')"/>
+                    <div class="px-1">-</div>
+                    <x-input-text name="zip22" id="zip22" class="w-20" :value="old('zip22')" placeholder='0001'
+                                 :error="$errors->get('zip22')" onKeyUp="{{ $on_keyup }}" />
+                </div>
+                <x-input-error :messages="$errors->get('zip21')" class="my-1"/>
+                <x-input-error :messages="$errors->get('zip22')" class="my-1"/>
+            @endif
+
+            @if($address_separate_type == App\Consts\CommonConst::ADDRESS_SEPARATE)
+                <div class="flex-start-center gap-2 mb-1">
+                    <x-input-text name="pref21" id="pref21" class="w-48" :value="old('pref21')" placeholder='東京都' :error="$errors->get('pref21')"/>
+                    <x-input-text name="address21" id="address21" class="w-48" :value="old('address21')"
+                                  :error="$errors->get('address21')"
+                                  placeholder='千代田区'/>
+                </div>
+                <x-input-error :messages="$errors->get('pref21')" class="my-1"/>
+                <x-input-error :messages="$errors->get('address21')" class="my-1"/>
+                <div>
+                    <x-input-text name="street21" id="street21" class="w-full" :value="old('street21')"
+                                  :error="$errors->get('street21')"
+                                  placeholder='千代田1-1-1マンション101号室'/>
+                </div>
+                <x-input-error :messages="$errors->get('street21')" class="my-1"/>
+            @else
+                <x-input-text name="address" class="w-full" :value="old('address')"
+                              :error="$errors->get('address')"
+                              placeholder="東京都千代田区千代田1-1-1マンション101号室"/>
+                <x-input-error :messages="$errors->get('address')" class="mt-1"/>
+            @endif
+        </div>
+    </div>
+</div>
