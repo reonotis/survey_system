0c2499306da64daaefd4374cb1c34b11e94920d5
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2026-02-28 01:17:10 +0900

    サンプルのHTMLメール送信機能作成

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
                 'resources/js/user/form/mail_setting.js',
                 'resources/js/user/form/message_setting.js',
                 'resources/js/user/form/register_item_setting.js',
+                'resources/js/user/form/sample_mail.js',
                 'resources/scss/user/form/item_setting.scss',
                 'resources/scss/welcome.scss',
             ],
